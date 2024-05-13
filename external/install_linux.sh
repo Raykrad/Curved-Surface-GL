e6e3ac44f4045b241def633e3503ab07b0768a16
Raykrad <109039576+Raykrad@users.noreply.github.com>
2024-05-13 20:05:46 +0300

    Add files via upload

---
+++ external/install_linux.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+git clone https://github.com/glfw/glfw.git
+echo 'glfw установка завешена'
+git clone https://github.com/g-truc/glm.git
+echo 'glm установка завешена'
+
+echo 'Необходимые компоненты установлены.'
---
+++ external/install_windows.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/glfw/glfw.git
+echo 'GLFW'
+
+git clone https://github.com/g-truc/glm.git
+echo 'GLM'
+
+echo 'Installation is over'
